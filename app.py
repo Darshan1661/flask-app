b314284bbe01e3b8d3bf3c268dcdb78295f05176
Darshan <darshansorthiya1661@gmail.com>
2025-04-06 08:14:13 +0530

    Updated multiple things

--- app.py
+++ app.py
@@ -193,5 +193,6 @@ def logout():
 
 # --- RUN APP ---
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or fallback
+    app.run(host="0.0.0.0", port=port)
+
